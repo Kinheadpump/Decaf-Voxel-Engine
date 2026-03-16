59c1f2addf339cf6aa866b6b2c6afd8c068afb01
Kinheadpump <67102569+Kinheadpump@users.noreply.github.com>
2026-03-16 23:29:26 +0100

    Config obj hinzugefügt

--- src/config.rs
+++ src/config.rs
@@ -1,18 +1,20 @@
 use serde::Deserialize;
 use std::fs;
 
-#[derive(Deserialize, Clone)]
+#[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    pub debug: DebugConfig,
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct DebugConfig {
     pub enable_profiler: bool,
 }
 
-impl DebugConfig {
+impl Config {
     pub fn load() -> Self {
-        let path = "config.toml";
-        let config_str =
-            fs::read_to_string(path).unwrap_or_else(|e| panic!("Failed to read {}: {}", path, e));
-
-        toml::from_str(&config_str)
-            .unwrap_or_else(|e| panic!("Failed to parse {}: {}", path, e))
+        let config_str = fs::read_to_string("config.toml")
+            .expect("Failed to read config.toml! Ensure the file exists in the project root.");
+        toml::from_str(&config_str).expect("Failed to parse config.toml!")
     }
 }
