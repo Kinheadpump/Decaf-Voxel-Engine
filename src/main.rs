9fbf75a193b2d77eb859bf62511f4e08c5e432a2
Kinheadpump <67102569+Kinheadpump@users.noreply.github.com>
2026-03-16 23:36:43 +0100

    github workflow added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![forbid(unsafe_op_in_unsafe_fn)]
+#![deny(unused_must_use)]
+#![warn(
+    clippy::dbg_macro,
+    clippy::todo,
+    clippy::unwrap_used,
+    clippy::undocumented_unsafe_blocks
+)]
+
 mod config;
 mod logging;
 
